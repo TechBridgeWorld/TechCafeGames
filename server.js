4d199fa4d6cd2f7d797b3298babda50490c827e9
hedgeday <sachin.hegde91@gmail.com>
2013-02-06 22:14:20 -0500

    server fixes

--- server.js
+++ server.js
@@ -6,80 +6,66 @@ app.use(express.bodyParser());
 app.use("/", express.static(__dirname + wwwDir));
 app.get("/", function(req, res) { res.render(wwwDir + "/index.html");});
 app.listen(8080);
-
-var https = require('http');
-var options = {
-    host    : 'http://server2.tbw.ri.cmu.edu',
-    method  : 'GET',
-    path    : '/CafeTeach/SilviaPessoa/data/qs-eau2rqip4l5inmroldp32ln755.xml'
-};
-var output = {};
 var xml2js = require('xml2js');
-var parser = new xml2js.Parser();
-var questionData = {};
-questionData['easy'] = [];
-questionData['medium'] = [];
-questionData['hard'] = [];
 
 function isEmpty(obj){
-    for(var i in obj){
-        if(obj.hasOwnProperty(i)){
-            return false;
-        }
-    }
-    return true;
-}
- 
-parser.on('end', function(result) {
-  numqs = result.list.m2qslist[0].m2qs.length;
-  //store each question in a Question object
-  var qs = new Array();
-  for (var i = 0; i < numqs; i++)
-  {	  
-	  var currentObj = {};
-	  currentObj['q'] = result.list.m2qslist[0].m2qs[i]["m2-qs"][0];
-	  currentObj['a'] = result.list.m2qslist[0].m2qs[i]["m2-ans"][0];
-	  currentObj['choices'] = []; 
-	  var temp = result.list.m2qslist[0].m2qs[i]["m2-opt"];
-	  for (var j = 0; j < temp.length; j++){
-	  		console.log(temp[j]);
-	  		if (!isEmpty(temp[j])){
-	  			currentObj['choices'].push(temp[j]);
-	  		}
-	  }
-	  if (result.list.m2qslist[0].m2qs[i]["m2-level"][0] == "easy"){
-			questionData['easy'].push(currentObj);
-	  }
-	  if (result.list.m2qslist[0].m2qs[i]["m2-level"][0] == "medium"){
-			questionData['medium'].push(currentObj);
-	  }
-	  if (result.list.m2qslist[0].m2qs[i]["m2-level"][0] == "hard"){
-			questionData['hard'].push(currentObj);
-	  }
-  }
-  console.log(questionData);
-  output = questionData;
-});
-
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-
-var xhr = new XMLHttpRequest();
-
-xhr.onreadystatechange = function() {
-	console.log("State: " + this.readyState);
-	
-	if (this.readyState == 4) {
-		console.log("Complete.\nBody length: " + this.responseText.length);
-		console.log('Output:');
-		parser.parseString(this.responseText);
+	for(var i in obj){
+		if(obj.hasOwnProperty(i)){
+			return false;
+		}
 	}
-};
+	return true;
+}
 
-xhr.open("GET", 'http://server2.tbw.ri.cmu.edu/CafeTeach/SilviaPessoa/data/qs-eau2rqip4l5inmroldp32ln755.xml');
-xhr.send();
-	
 app.get("/register", function(req,res){
-	res.send(output);
+	var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+	var xhr = new XMLHttpRequest();
+
+	xhr.onreadystatechange = function() {
+		console.log("State: " + this.readyState);
+		
+		if (this.readyState == 4) {
+			console.log("Complete.\nBody length: " + this.responseText.length);
+			console.log('Output:');
+			parser.parseString(this.responseText);
+		}
+	};
+	var parser = new xml2js.Parser();
+	xhr.open("GET", 'http://server2.tbw.ri.cmu.edu/CafeTeach/SilviaPessoa/data/qs-eau2rqip4l5inmroldp32ln755.xml');
+	xhr.send();
+
+	parser.on('end', function(result) {
+		var numqs = result.list.m2qslist[0].m2qs.length;
+		var questionData = {};
+		questionData['easy'] = [];
+		questionData['medium'] = [];
+		questionData['hard'] = [];
+
+	    for (var i = 0; i < numqs; i++)
+	    {	  
+	  		var currentObj = {};
+		  	currentObj['q'] = result.list.m2qslist[0].m2qs[i]["m2-qs"][0];
+		  	currentObj['a'] = result.list.m2qslist[0].m2qs[i]["m2-ans"][0];
+		  	currentObj['choices'] = []; 
+		  	var temp = result.list.m2qslist[0].m2qs[i]["m2-opt"];
+		  	for (var j = 0; j < temp.length; j++){
+		  		if (!isEmpty(temp[j])){
+		  			currentObj['choices'].push(temp[j]);
+		  		}
+		  	}
+		  	if (result.list.m2qslist[0].m2qs[i]["m2-level"][0] == "easy"){
+		  		questionData['easy'].push(currentObj);
+		  	}
+		  	if (result.list.m2qslist[0].m2qs[i]["m2-level"][0] == "medium"){
+		  		questionData['medium'].push(currentObj);
+		  	}
+		  	if (result.list.m2qslist[0].m2qs[i]["m2-level"][0] == "hard"){
+		  		questionData['hard'].push(currentObj);
+		  	}
+	  }
+	  console.log(questionData);
+	  res.send(questionData);
+	});
 });
 
 
