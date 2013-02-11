82e48138881f89833f5999e38dd3b8bebc3cd613
hedgeday <sachin.hegde91@gmail.com>
2013-02-11 18:23:52 -0500

    Revamped game

--- js/game.js
+++ js/game.js
@@ -1,11 +1,7 @@
-//Replace these values with height and width retrieved from phone
-
 window.onload = execute();
 
-var questionData = {};
-
-function execute() {
-    // Crafty.init($(window).width(), $(window).height());
+function execute(){
+    var questionData = {};
     
     var ajaxRequest = function(url, fnSuccess, fnError){
         $.ajax({
@@ -17,74 +13,167 @@ function execute() {
     };
 
     ajaxRequest( 
-                '/register', 
-                function onSuccess(data){
-                    if(data)
-                    {   
-                    //     console.log('data:');
-                    //     console.log(data['list']);
-                    //     console.log('stringify:');
-                        questionData = data; 
-                        // console.log(JSON.stringify(questionData));
-                    }
-                },
-                function onError(data){
-                    // console.log(JSON.stringify(data));
-    });
+        '/register', 
+        function onSuccess(data){
+            if(data)
+                {   
+                    questionData = data; 
+                }
+            },
+        function onError(data){ 
+            }
+    );
 
     var width = window.innerWidth;
-    var height = window.innerHeight;
-    
-    var stage = new Kinetic.Stage({
-        container: 'gameCanvas',
-        width: width, 
-        height: height,
-    });
+    var windowHeight = window.innerHeight;
+    var height = 0.9*windowHeight;
+    var road;
+    var road2; 
+    var car; 
+    var carPos; 
+    var carLayer; 
+    var roadLayer;
+    var stage;
+    var interval;
+    var ctx;
+    var roadY; 
+    var road2Y;
+    var canvas;  
+    var carWidth; 
+    var carHeight;
+    var score;  
+    var carMargin; 
+    var questionInterval;
+    var questionFlag = false;
+    var obstacle;
+    var obstacleInterval;
+    var roadImage = new Image();
+    roadImage.src = "img/race-assets/track.png";
 
-    var layer = new Kinetic.Layer(); 
-
-    var rect = new Kinetic.Rect({
-        x:0,
-        y:0,
-        width: width, 
-        height: height, 
-        fill: 'yellow'
-    });
-
-    layer.add(rect); 
-
-    var image = new Image(); 
-    image.src = "img/road_vert.png";
-    image.onload = function(){
-        var roadY = 0; 
-        var road2Y = -height;
-        var road = new Kinetic.Image({
-            x:0, 
-            y:0,
-            image:image, 
-            width:width, 
-            height:height 
-        });
-        var road2 = new Kinetic.Image({
-            x:0, 
-            y:-height,
-            image:image, 
-            width:width, 
-            height:height 
-        });
-        layer.add(road2);
-        layer.add(road);
-        stage.add(layer);
-
-        var anim = new Kinetic.Animation(function(frame){
-            road.setY(roadY += 60);
-            if (roadY >= height){roadY = -height;}
-            road2.setY(road2Y += 60); 
-            if (road2Y >= height){ road2Y = -height;}
-        }, layer); 
-        anim.start();
-    };
+    var carImage = new Image(); 
+    carImage.src = "img/race-assets/car.png";
 
 
-};
-  
+    var questionBoxImage = new Image();
+    questionBoxImage.src = "img/race-assets/question-bg.png";
+
+    var coneImage = new Image(); 
+    coneImage.src = "img/race-assets/obstacle-c.png";
+
+    console.log(questionData);
+    function setup(){    
+        score = 0;
+        $('#headerDiv').height('' + 0.1*windowHeight + 'px');
+        carWidth = 0.1*width; 
+        carHeight = 0.2*width;
+        carMargin = height - 1.5*carHeight;
+        carPos = width/2 - 0.05 * width;
+        canvas = document.getElementById('gameCanvas');
+        ctx = $('#gameCanvas')[0].getContext('2d');
+        ctx.canvas.width = width; 
+        ctx.canvas.height = height;
+        ctx.fillStyle = '#ffffff';
+        ctx.fillRect(0,0,width,height); 
+        roadY = 0; 
+        road2Y = -height; 
+        // obstacles = [];
+        // numObstacles = 0;
+        canvas.addEventListener('touchmove', setupEventListener, false);
+
+        drawRoad();
+        
+        interval = setInterval(draw, 50);
+
+        // obstacleInterval = setInterval(addObstacle, 2000);
+
+        questionInterval = setInterval(function(){
+            questionFlag = true;
+            window.clearInterval(obstacleInterval);
+            setTimeout(function(){
+                questionFlag = false;
+                // obstacleInterval = setInterval(addObstacle, 1000);
+                canvas.addEventListener('touchmove', setupEventListener, false);
+            }, 10000);
+        }, 20000);
+    }
+
+    setup();
+
+    function setupEventListener(event){
+        for (var i = 0; i < event.touches.length; i++) {
+            var touch = event.touches[i];
+            carPos = touch.pageX;
+            if (carPos <= width/5) carPos = width/5;
+            if (carPos >= width-width/3.6) carPos = width - width/3.6;              
+        }
+    }
+
+    function draw(){
+        drawRoad();
+        drawCar();
+        if (questionFlag) drawQuestionBox();
+        else {
+            drawScore();
+            // drawObstacles();
+        }
+    }
+
+    function drawRoad(){
+        if (roadY >= height)
+            {
+                roadY = road2Y-height;
+            }
+        roadY += 50;
+        if (road2Y >= height)
+            { 
+                road2Y = roadY-height;
+            }
+        road2Y += 50;
+        ctx.drawImage(roadImage, 0, road2Y, width, height);
+        ctx.drawImage(roadImage, 0, roadY, width, height);
+    }
+
+    function drawCar(){
+        ctx.drawImage(carImage, carPos, carMargin, carWidth, carHeight);
+    }  
+
+    function drawScore(){
+        $('#score').text('Score: '+score);
+        score++;
+    }
+
+    function drawQuestionBox(){
+        canvas.removeEventListener('touchmove', setupEventListener, false);
+        ctx.drawImage(questionBoxImage, 0.1*width, 0.1*height, 0.8*width, 0.8*height);
+        ctx.textAlign = 'center';
+        ctx.fillText('question',width/2,height/4);
+        ctx.fillText('answer1', width/3.5,height/2);
+        ctx.fillText('answer2', width*0.7, height/2);
+        ctx.fillText('answer3', width/3.5, height*0.75);
+        ctx.fillText('answer4', width*0.7, height*0.75);
+    }
+
+    // function addObstacle(){
+    //     var rand = Math.floor(Math.random()*3);
+    //     var obs = new Object(); 
+    //     obs.y = 0; 
+    //     if (rand == 0)
+    //         obs.x = width/4; 
+    //     if (rand == 1)
+    //         obs.x = width/2; 
+    //     if (rand == 2)
+    //         obs.x = width*0.70;
+    //     obstacles.push(obs);
+    //     numObstacles++;
+    // }
+
+    // function drawObstacle(){
+    //     for (var i = 0; i < obstacles.length;i++){
+    //         ctx.drawImage(coneImage, obstacles[i].x, obstacles[i].y,carWidth,carWidth);
+    //         obstacles[i].y+=10;
+    //     }
+    // }
+
+}
+
+
