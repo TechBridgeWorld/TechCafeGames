68dc11fc473d665b9250ec5b617cd51e28e6ad26
hedgeday <sachin.hegde91@gmail.com>
2013-02-15 18:16:25 -0500

    css background scroll, bug fixes, cleanup, and slight restructuring

--- js/game.js
+++ js/game.js
@@ -26,11 +26,11 @@ function execute(){
 
     var width = window.innerWidth;
     var windowHeight = window.innerHeight;
-    var height = 0.9*windowHeight;
+    var height = 0.9*windowHeight + 20;
     var road;
     var road2; 
     var car; 
-    var carPos; 
+    var carX; 
     var carLayer; 
     var roadLayer;
     var stage;
@@ -41,17 +41,21 @@ function execute(){
     var canvas;  
     var carWidth; 
     var carHeight;
+    var obstacleHeight; 
     var score;  
-    var carMargin; 
+    var carY; 
     var questionInterval;
     var questionFlag = false;
     var obstacle;
     var allObstacles=["cone", "coin", "manhole"];
     var obsArr=[];
-    var timer = 0;
+    var timer;
     var allPoints=[-10, 20, -20];
     var obstacleInterval;
     var roadImage = new Image();
+    var lane1X; 
+    var lane2X; 
+    var lane3X;
     roadImage.src = "img/race-assets/track.png";
 
     var carImage = new Image(); 
@@ -70,31 +74,35 @@ function execute(){
     var manholeImage = new Image(); 
     manholeImage.src = "img/race-assets/obstacle-m.png";
 
-    console.log(questionData);
     function setup(){    
+        
+        /* General variables */
+        timer = 0; 
         score = 0;
+        
+        /* Canvas+DOM variables */
         $('#headerDiv').height('' + 0.1*windowHeight + 'px');
-        carWidth = 0.1*width; 
-        carHeight = 0.2*width;
-        carMargin = height - 1.5*carHeight;
-        carPos = width/2 - 0.05 * width;
         canvas = document.getElementById('gameCanvas');
         ctx = $('#gameCanvas')[0].getContext('2d');
         ctx.canvas.width = width; 
         ctx.canvas.height = height;
-        ctx.fillStyle = '#ffffff';
-        ctx.fillRect(0,0,width,height); 
-        roadY = 0; 
-        road2Y = -height; 
-         obstacles = [];
-         numObstacles = 0;
-        canvas.addEventListener('touchmove', setupEventListener, false);
+        ctx.clearRect(0,0,width,height); 
+        
+        /* Car variables */
+        carWidth = 0.1*width; 
+        carHeight = 0.2*width;
+        carY = height - 1.5*carHeight;
+        carX = width/2 - 0.05 * width;
+        
+        /* Obstacle Variables */
+        obstacleHeight = carHeight/2;
+        lane1X = width/4-carWidth/2;
+        lane2X = width/2-carWidth/2; 
+        lane3X = width*0.70-carWidth/2;
 
-        drawRoad();
+        canvas.addEventListener('touchmove', setupEventListener, false);
         
         interval = setInterval(draw, 50);
-
-        //obstacleInterval = setInterval(addObstacle, 2000);
 
         questionInterval = setInterval(function(){
             questionFlag = true;
@@ -102,97 +110,51 @@ function execute(){
             window.clearInterval(obstacleInterval);
             setTimeout(function(){
                 questionFlag = false;
-                //obstacleInterval = setInterval(addObstacle, 1000);
                 canvas.addEventListener('touchmove', setupEventListener, false);
             }, 10000);
         }, 20000);
     }
 
+    function Obs(name, points, x, y) {
+        this.name = name;
+        this.points = points;
+        this.x=x;
+        this.y=y;
+        this.eaten=false;
+    }
+
+
+    Obs.prototype.update = function(){
+        this.y+=10;
+        if(this.x > carX-50 && this.x < carX+50 && this.y+obstacleHeight >= carY) {
+            this.eaten=true;
+        }
+    }
+
     setup();
 
     function setupEventListener(event){
+        event.preventDefault();   
         for (var i = 0; i < event.touches.length; i++) {
             var touch = event.touches[i];
-            carPos = touch.pageX;
-            if (carPos <= width/5) carPos = width/5;
-            if (carPos >= width-width/3.6) carPos = width - width/3.6;              
+            carX = touch.pageX;
+            if (carX <= width/5) carX = width/5;
+            if (carX >= width-width/3.6) carX = width - width/3.6;              
         }
     }
 
     function draw(){
-        drawRoad();
+        ctx.clearRect(0,0,width,height); 
         drawCar();
         if (questionFlag) drawQuestionBox();
         else {
             drawScore();
-            //**obstacles**
-            timer++;
-            if (timer%150 === 0) {
-                var index = Math.floor(Math.random()*(allObstacles.length));
-                var x;
-                var rand = Math.floor(Math.random()*3);
-         if (rand == 0)
-             x = width/4-carWidth/2; 
-         if (rand == 1)
-             x = width/2-carWidth/2; 
-         if (rand == 2)
-             x = width*0.70-carWidth/2;
-                obsArr.push(new Obs(allObstacles[index], allPoints[index], x, 0));
-            }
-            
-            for(var i = 0; i < obsArr.length; i++) {
-            obsArr[i].update(carPos, carMargin);
-            if(obsArr[i].eaten) {
-                if(obsArr[i].points >= 0) {
-                    $("body").append('<div id="pts"><p>+'+obsArr[i].points+'</p></div>');
-                    $("#pts").css("color","green");
-                }
-                else {
-                    $("body").append('<div id="pts"><p>'+obsArr[i].points+'</p></div>');
-                    $("#pts").css("color","red");
-                }
-                $("body").css("position","relative");
-                $("#pts").css("margin-top",-(height-obsArr[i].y)-25);
-                $("#pts").css("margin-left",obsArr[i].x-25);
-                $("#pts").css("position","relative");
-                $("#pts").css("z-index",10);
-                $("#pts").css("font-size",30);
-                window.setTimeout(function(){$("#pts").remove();}, 1000);
-                obsArr.splice(i,1);
-            }
-            else if(obsArr[i].y >= height) {
-                obsArr.splice(i,1);
-            }
-            else 
-            {
-                if(obsArr[i].name == "cone") ctx.drawImage(coneImage, obsArr[i].x, obsArr[i].y, carWidth, carHeight/2);
-                
-                if(obsArr[i].name == "coin") ctx.drawImage(coinImage, obsArr[i].x, obsArr[i].y, carWidth, carHeight/2);
-                
-                if(obsArr[i].name == "manhole") ctx.drawImage(manholeImage, obsArr[i].x, obsArr[i].y, carWidth, carHeight/2);
-            }
-            }
-            //****
+            drawObstacles();
         }
     }
 
-    function drawRoad(){
-        if (roadY >= height)
-            {
-                roadY = road2Y-height;
-            }
-        roadY += 50;
-        if (road2Y >= height)
-            { 
-                road2Y = roadY-height;
-            }
-        road2Y += 50;
-        ctx.drawImage(roadImage, 0, road2Y, width, height);
-        ctx.drawImage(roadImage, 0, roadY, width, height);
-    }
-
     function drawCar(){
-        ctx.drawImage(carImage, carPos, carMargin, carWidth, carHeight);
+        ctx.drawImage(carImage, carX, carY, carWidth, carHeight);
     }  
 
     function drawScore(){
@@ -211,30 +173,53 @@ function execute(){
         ctx.fillText('answer4', width*0.7, height*0.75);
     }
 
-
-
-     /*function addObstacle(){
-         var rand = Math.floor(Math.random()*3);
-         var obs = new Object(); 
-         obs.y = 0; 
-         if (rand == 0)
-             obs.x = width/4; 
-         if (rand == 1)
-             obs.x = width/2; 
-         if (rand == 2)
-             obs.x = width*0.70;
-          obstacles.push(obs);
-         numObstacles++;
-     }
-
-     function drawObstacle(){
-         for (var i = 0; i < obstacles.length;i++){
-             console.log(i);
-             ctx.drawImage(coneImage, obstacles[i].x, obstacles[i].y,carWidth,carWidth);
-             obstacles[i].y+=10;
-         }
-     }*/
-
+    function drawObstacles(){
+        timer++;
+        if (timer%150 === 0) {
+            var index = Math.floor(Math.random()*(allObstacles.length));
+            var x;
+            var rand = Math.floor(Math.random()*3);
+            if (rand == 0)
+                x = width/4-carWidth/2; 
+            else if (rand == 1)
+                x = width/2-carWidth/2; 
+            else if (rand == 2)
+                x = width*0.70-carWidth/2;
+            obsArr.push(new Obs(allObstacles[index], allPoints[index], x, -obstacleHeight));
+        }   
+            
+        for(var i = 0; i < obsArr.length; i++) {
+        obsArr[i].update(carX, carY);
+        if(obsArr[i].eaten) {
+            if(obsArr[i].points >= 0) {
+                $("body").append('<div id="pts"><p>+'+obsArr[i].points+'</p></div>');
+                $("#pts").css("color","green");
+            }
+            else {
+                $("body").append('<div id="pts"><p>'+obsArr[i].points+'</p></div>');
+                $("#pts").css("color","red");
+            }
+            $("#pts").css("margin-top",-(height-obsArr[i].y)-25);
+            $("#pts").css("margin-left",obsArr[i].x-25);
+            $("#pts").css("position","relative");
+            $("#pts").css("z-index",10);
+            $("#pts").css("font-size",30);
+            window.setTimeout(function(){$("#pts").remove();}, 1000);
+            obsArr.splice(i,1);
+        }
+        else if(obsArr[i].y >= height) {
+            obsArr.splice(i,1);
+        }
+        else {
+            if(obsArr[i].name == "cone") 
+                ctx.drawImage(coneImage, obsArr[i].x, obsArr[i].y, carWidth, obstacleHeight);       
+            if(obsArr[i].name == "coin") 
+                ctx.drawImage(coinImage, obsArr[i].x, obsArr[i].y, carWidth, obstacleHeight);
+            if(obsArr[i].name == "manhole") 
+                ctx.drawImage(manholeImage, obsArr[i].x, obsArr[i].y, carWidth, obstacleHeight);
+            }
+        }
+    }
 }
 
 
