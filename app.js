deb8292a9836d3a4b32936a6b60317cf17f9758a
Erik Pintar <elpintar@gmail.com>
2013-05-05 22:28:23 -0400

    more fixes

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var fs = require("fs");
 var path = require('path');
 
 var assert = require('./assertNode.js');
- var techcafe = require('./node_modules/node_techcafe/node_techcafe');
 
 // for questions Erik made
 var easyEnglishQuestionsPath =
@@ -548,5 +547,130 @@ function initServer() {
 
     _initUserAuth();
 }
+
+
+/*
+    TECH CAFE MODULE
+*/
+
+// wrapped techcafe node module in a closure
+
+var techcafe = (function() {
+    // this is what is exported
+    var module = {};
+    // other req's
+    var http = require('http');
+    var events = new require('events');
+    var eventResp = new events.EventEmitter;
+    eventResp.setMaxListeners(0);
+
+    var options = {  
+        host: 'techcafe-teacher.herokuapp.com', 
+        method: 'GET',
+    };
+
+    function setListener(event, callback){
+        if (str === undefined){
+            eventResp.once(event, function(data){
+                if (data !== 'null'){ 
+                    try{
+                        callback(JSON.parse(data));
+                    }
+                    catch(err){
+                        callback("Error here");
+                    }
+                }
+                else{
+                    callback("No data found.");
+                }
+            });
+            eventResp.once('error', function(err){
+                callback('Error, internal kind: ' + err);
+            });
+        }
+        else{
+            callback(str);
+        }
+    }
+
+    var str; 
+
+    // Returns a list of all teachers in the database
+    // Format: [{username:Teacher1}, 
+    //          {username:Teacher2},...]
+    module.getTeacherList = function(callback){
+        var opt = options; 
+        opt.path = '/users/teacher_list.json';
+        
+        http.request(opt,function(response){
+            var str = ''; 
+            response.on('error', function(err){
+                console.log('Error: ' + err);
+                eventResp.emit('error', err);
+            });
+            response.on('data', function (chunk) 
+                { str += chunk; }
+            );
+            response.on('end', function () { 
+                eventResp.emit('completeTeacherList', str);
+            });
+        }).end();
+
+        setListener('completeTeacherList',callback);
+    }
+
+    // Returns a list of all content sets in the database
+    //Format: {ContentSet1: Questions, ContentSet2: Questions, ...}
+    module.getContentSetList = function(callback){
+        var opt = options; 
+        opt.path = '/content_sets/get_content_sets.json';
+        
+        http.request(opt,function(response){
+            var str = ''; 
+            response.on('error', function(err){
+                console.log('Error: ' + err);
+                eventResp.emit('error', err);
+            });
+            response.on('data', function (chunk) 
+                { str += chunk; }
+            );
+            response.on('end', function () { 
+                eventResp.emit('completeContentSetList', str);
+            });
+        }).end();
+
+        setListener('completeContentSetList', callback);
+    }
+
+    //Returns JSON containing all content sets for given teacher ID
+    // Format: {
+    //          username: 'username', content_sets: 
+    //          [{name:ContentSet1, questions:Questions},
+    //          {name:ContentSet2, questions:Questions}, ...]
+    //          }
+    module.getContentByTeacher = function(tid, callback){
+        var opt = options; 
+        opt.path = '/users/teacher_content.json?name='+tid+'&include_content_sets=true';
+        
+        http.request(opt,function(response){
+            var str = ''; 
+            response.on('error', function(err){
+                console.log('INTERNAL Error: ' + err);
+                eventResp.emit('inside error', err);
+            });
+            response.on('data', function (chunk) 
+                { str += chunk; }
+            );
+            response.on('end', function () { 
+                eventResp.emit('teacherContent'+tid, str);
+            });
+        }).end();
+
+        setListener('teacherContent'+tid, callback);    
+    }
+
+    return module;
+}());
+
 
 initServer();
